681f921577ab51d13e46f13fd5fb42c177783559
Семен <igoshinsemen08@gmail.com>
2022-07-11 17:28:26 +0300

    работа с ускорением

--- script.js
+++ script.js
@@ -16,7 +16,23 @@ function oneClick() {
 function speed12() {
     if (clicker >= 100) {
         clicker = clicker - 100;
-        speed = speed * 1.2;
+        speed = speed * 0.001;
+        let timerId = setInterval(() => click(), speed);    
+    }
+};
+
+function speed13() {
+    if (clicker >= 1000) {
+        clicker = clicker - 1000;
+        speed = speed * 0.7;
+        let timerId = setInterval(() => click(), speed);    
+    }
+};
+
+function speed14() {
+    if (clicker >= 10000) {
+        clicker = clicker - 10000;
+        speed = speed * 0.1;
         let timerId = setInterval(() => click(), speed);    
     }
 };
