cce2eed5db8dfc3eebc5513f74dc10cd5c97ab42
Семен <igoshinsemen08@gmail.com>
2022-07-13 18:04:38 +0300

    добавил мобильную версию

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ let timerId = setInterval(() => click(), speed);
 // Добаляет один поинт к цели
 
 function click() {
-    if (clicker >= 1000000) {
+    if (clicker >= 10000) {
         alert("Good job! Take a cake.");
         let timerId = setInterval(() => stop(), 0);
     }
     
     clicker++;
     document.getElementById("counter").innerHTML = clicker;
-    document.getElementById("goal").innerHTML = 1000000-clicker;
+    document.getElementById("goal").innerHTML = 10000-clicker;
 };
 
 function oneClick() {
     clicker = clicker + 1;
     document.getElementById("counter").innerHTML = clicker;
-    document.getElementById("goal").innerHTML = 1000000-clicker;
+    document.getElementById("goal").innerHTML = 10000-clicker;
 };
 
 function speed12() {
-    if (clicker >= 100) {
-        clicker = clicker - 100;
+    if (clicker >= 10) {
+        clicker = clicker - 10;
         speed = speed * 0.8;
         let timerId = setInterval(() => click(), speed);    
     }
 };
 
 function speed13() {
-    if (clicker >= 1000) {
-        clicker = clicker - 1000;
+    if (clicker >= 100) {
+        clicker = clicker - 100;
         speed = speed * 0.7;
         let timerId = setInterval(() => click(), speed);    
     }
 };
 
 function speed14() {
-    if (clicker >= 10000) {
-        clicker = clicker - 10000;
+    if (clicker >= 1000) {
+        clicker = clicker - 1000;
         speed = speed * 0.1;
         let timerId = setInterval(() => click(), speed);    
     }
